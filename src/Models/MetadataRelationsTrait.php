66729655b231d18d94f738a156018453b56a45ac
c-harris <c.harris@communicationinnovations.com.au>
2020-02-24 18:00:23 +0000

    Apply fixes from StyleCI

--- src/Models/MetadataRelationsTrait.php
+++ src/Models/MetadataRelationsTrait.php
@@ -80,9 +80,9 @@ trait MetadataRelationsTrait
         return !empty($rels['UnknownPolyMorphSide']);
     }
     /**
-     * @param \ReflectionMethod $method
-     * @return string
+     * @param  \ReflectionMethod         $method
      * @throws InvalidOperationException
+     * @return string
      */
     protected function getCodeForMethod(\ReflectionMethod $method) : string
     {
