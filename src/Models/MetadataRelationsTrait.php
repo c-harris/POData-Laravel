bab8eceb7f8fe8e60943671fb21988643bb4785a
c-harris <c.harris@communicationinnovations.com.au>
2020-02-27 00:53:44 +1000

    consistent access by self

--- src/Models/MetadataRelationsTrait.php
+++ src/Models/MetadataRelationsTrait.php
@@ -32,6 +32,6 @@ trait MetadataRelationsTrait
      */
     public function getRelationships()
     {
-        return static::$relationNames =  self::$relationNames ?? ModelReflectionHelper::getRelationshipsFromMethods(/** @scrutinizer ignore-type */$this);
+        return self::$relationNames =  self::$relationNames ?? ModelReflectionHelper::getRelationshipsFromMethods(/** @scrutinizer ignore-type */$this);
     }
 }
