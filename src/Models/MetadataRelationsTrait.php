62971b334a078e556f6a8d8a3d37256db2a3b675
c-harris <c.harris@communicationinnovations.com.au>
2020-02-27 00:35:24 +1000

    fix scrutiniser bug

--- src/Models/MetadataRelationsTrait.php
+++ src/Models/MetadataRelationsTrait.php
@@ -32,6 +32,6 @@ trait MetadataRelationsTrait
      */
     public function getRelationships()
     {
-        return static::$relationNames =  static::$relationNames ?? ModelReflectionHelper::getRelationshipsFromMethods($this);
+        return static::$relationNames =  self::$relationNames ?? ModelReflectionHelper::getRelationshipsFromMethods(/** @scrutinizer ignore-type */$this);
     }
 }
