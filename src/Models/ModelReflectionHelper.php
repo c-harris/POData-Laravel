5764abb61566dd414d0ef44f80040240d18a287a
c-harris <c.harris@communicationinnovations.com.au>
2020-02-27 08:22:29 +0000

    Apply fixes from StyleCI

--- src/Models/ModelReflectionHelper.php
+++ src/Models/ModelReflectionHelper.php
@@ -27,7 +27,7 @@ abstract class ModelReflectionHelper
     ];
 
     /**
-     * @param  ReflectionMethod         $method
+     * @param  ReflectionMethod $method
      * @return string
      */
     public static function getCodeForMethod(ReflectionMethod $method): string
@@ -64,9 +64,9 @@ abstract class ModelReflectionHelper
     }
 
     /**
-     * @param Model $model
-     * @return array|string[]
+     * @param  Model               $model
      * @throws ReflectionException
+     * @return array|string[]
      */
     public static function getRelationshipsFromMethods(Model $model): array
     {
